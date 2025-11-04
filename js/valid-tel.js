5f10b84e1ba067f881ea6ae6caf738c031e67034
Mykola Hrechkosii <60157425+Hrechkosii-Nikolay@users.noreply.github.com>
2025-11-04 12:47:25 +0200

    final fix

--- js/valid-tel.js
+++ js/valid-tel.js
@@ -1,15 +1,1 @@
-
-  const form = document.getElementById('form');
-  const phoneInput = document.getElementById('input-tel');
-
-  form.addEventListener('submit', function (e) {
-    // Видаляємо всі нецифрові символи
-    const phoneValue = phoneInput.value.replace(/\D/g, '');
-
-    // Перевіряємо, чи номер має 12 цифр (наприклад: +38 (096) 123-45-67 => 380961234567)
-    if (phoneValue.length < 12) {
-      e.preventDefault(); // Блокуємо відправку
-      alert('Будь ласка, введіть повний номер телефону');
-    }
-  });
-
+const form=document.getElementById("form"),phoneInput=document.getElementById("input-tel");form.addEventListener("submit",function(e){let t=phoneInput.value.replace(/\D/g,"");t.length<12&&(e.preventDefault(),alert("Будь ласка, введіть повний номер телефону"))});
